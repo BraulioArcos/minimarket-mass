8d98cd7dd6b85107bae75067a40255521ee8475c
Josias Yu <1588052@senati.pe>
2026-05-29 13:52:03 -0500

    Subiendo avance de clase 1.51 pm

--- clases/Producto.php
+++ clases/Producto.php
@@ -6,5 +6,17 @@ class Producto {
     public string $nombre;
     public float $precio;
     public int $stock;
+
+    public function __construct(
+        string $codigo,
+        string $nombre,
+        float $precio,
+        int $stock
+    ) {
+        $this->codigo = $codigo;
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->stock  = $stock;
+    }
 }
 ?>
--- test_producto.php
+++ test_producto.php
@@ -2,16 +2,19 @@
 declare(strict_types=1);
 require_once 'clases/Producto.php';
 
-// Crear el objeto
-$incaKola = new Producto();
-
-// Asignar valores a sus propiedades con el operador ->
-$incaKola->codigo = 'INC500';
-$incaKola->nombre = 'Inca Kola 500ml';
-$incaKola->precio = 3.50;
-$incaKola->stock  = 48;
+// Crear el objeto y Asignar Valores
+$incaKola     = new Producto('INC500', 'Inca Kola 500ml', 3.50, 48);
+$scotchBrite  = new Producto('SCB400', 'Scotch Brite 3uds', 4.50, 30);
+$inkaChips    = new Producto('IKC300', 'Inca Chips 135g', 2.50, 10);
+$kolaEscocesa = new Producto('KLE200', 'Kola Escocesa 355ml', 4.50, 5);
 
 // Leer las propiedades igual: con ->
 echo $incaKola->nombre;   // Inca Kola 500ml
-echo $incaKola->precio;   // 3.5
+echo "<br>";
+echo $scotchBrite->nombre;  
+echo "<br>";
+echo $inkaChips->nombre;
+echo "<br>";
+echo $kolaEscocesa->nombre;
+
 ?>
